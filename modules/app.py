cd8b8de7ce46158612875d15e0284a821e8f7989
gjburke2 <gjburke2@illinois.edu>
2024-06-07 12:18:23 -0500

    Added labelling based on model predictions

--- modules/app.py
+++ modules/app.py
@@ -1,7 +1,31 @@
-from get_emails import get_emails
+import pickle
+from email_interaction import get_emails
+from email_interaction import label_emails
+from process_emails import process_emails
 
 def main():
-    print(get_emails(2))
+    # get emails, process them
+    ids, emails = get_emails(200)
+    processed_emails = process_emails(emails)
+    # load the model
+    with open("../model.pkl","rb") as f:
+        model = pickle.load(f)
+    # predict with model
+    predictions = model.predict(processed_emails)
+    # output results
+    for i in range(0, len(emails), 1):
+        match predictions[i]:
+            case 0:
+                print("Not a college email")
+            case 1:
+                print("A college email")
+            case _:
+                print("Something went wrong")
+        print("Email id: " + str(ids[i]) + " - " + str(emails[i][:100]))
+    # label as college email based on output
+    print(predictions)
+    print(ids)
+    label_emails(predictions, ids, "test-college-email")
 
 if __name__ == "__main__":
     main()
--- modules/email_interaction.py
+++ modules/email_interaction.py
@@ -12,13 +12,11 @@ from nltk.stem import PorterStemmer
 nltk.download('punkt')
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/gmail.modify"]
 
-
+# Takes in number of emails to scan, outputs a list of [ids, emails] which have a body and stems all the words
+# and separates each email into a list of words
 def get_emails(num_emails):
-  """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
-  """
   creds = None
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
@@ -38,8 +36,9 @@ def get_emails(num_emails):
     with open("../token.json", "w") as token:
       token.write(creds.to_json())
 
-  # Create list of all the emails
+  # Create list of all the emails and their 
   emails = []
+  ids = []
 
   try:
     # Call the Gmail API and get messages
@@ -49,12 +48,16 @@ def get_emails(num_emails):
 
     # For now, print the messages
     for email_id in email_ids:
-      message = service.users().messages().get(userId="me", id=email_id['id'], format='full').execute()
+      try:
+        email = service.users().messages().get(userId="me", id=email_id['id'], format='full').execute()
 
-      if message.get('payload').get('body').get('data'):
-        # Get body
-        decoded_data = base64.urlsafe_b64decode(message.get('payload').get('body').get('data').encode('ASCII').decode('utf-8'))
-        raw_body = str(BeautifulSoup(decoded_data, "lxml").body())
+        # Get body or snippet
+        raw_body = email.get("snippet")
+        if email.get("payload").get("body").get("data"):
+          decoded_data = base64.urlsafe_b64decode(email.get("payload").get("body").get("data").encode("ASCII")).decode("utf-8")
+          raw_body = str(BeautifulSoup(decoded_data, "lxml").body())
+
+        ids.append(email_id['id'])
   
         # Cleaning up everything 
         body = raw_body.lower()
@@ -94,14 +97,70 @@ def get_emails(num_emails):
           i += 1
         words = list(filter(None, words))
         emails.append(words)
-      else:
-        print(f"No body found for Email ID {email_id['id']}.")
+      except Exception as error:
+        print(error)
       
   except HttpError as error:
-    # TODO(developer) - Handle errors from gmail API.
+    # Handle errors from gmail API.
     print(f"An error occurred: {error}")
 
-  return emails
+  return [ids, emails]
+
+def label_emails(predictions, ids, label_name):
+  if len(predictions) != len(ids):
+    print("Labels and ids don't match")
+    return
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("../token.json"):
+    creds = Credentials.from_authorized_user_file("../token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "../credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("../token.json", "w") as token:
+      token.write(creds.to_json())
+  # Change the emails tag_name based on the label
+  try:
+    # Call the Gmail API and get messages
+    service = build("gmail", "v1", credentials=creds)
+    # Create the label to use if not already present
+    new_label = {
+      'name': label_name,
+      'messageListVisibility': 'show',
+      'labelListVisibility': 'labelShow',
+    } 
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+    contains = False
+    for label in labels:
+      if label['name'] == label_name:
+        contains = True
+    if not contains:
+      service.users().labels().create(userId='me', body=new_label).execute()
+    # Get the label id as to add it
+    label_id = ""
+    for label in labels:
+      if label['name'] == label_name:
+        label_id = label['id']
+    # Iterate over all the emails ids
+    print("label_id = " + str(label_id))
+    for i in range(0, len(predictions), 1):
+      print(predictions[i])
+      if predictions[i] == 1:
+        print("Adding label")
+        service.users().messages().modify(userId='me', id=ids[i], body={'addLabelIds': [label_id]}).execute()
+  except HttpError as error:
+    # Handle errors from gmail API.
+    print(f"An error occurred: {error}")
 
 #if __name__ == "__main__":
 #  print(get_emails(5))
